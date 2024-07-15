7e893eb7a8f3d5501ab13a4a411ace9267b35f84
cnyaga66 <cnyga66@gmail.com>
2024-07-16 01:19:23 +0300

    commiit

--- server/models.py
+++ server/models.py
@@ -1,14 +1,14 @@
+# server/models.py
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
-
+class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140), nullable=False)
+    username = db.Column(db.String(64), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f"Message('{self.body}', '{self.username}')"
